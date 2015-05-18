c8498c2e62dcae9c550df5ae59c1cdc27a098317
Zizaco <zizaco@gmail.com>
2015-05-18 00:12:05 -0300

    Fixed minor typo

--- src/Mongolid/DynamicSchema.php
+++ src/Mongolid/DynamicSchema.php
@@ -4,7 +4,7 @@ namespace Mongolid;
 /**
  * The DynamicSchema will accept additional fields that are not specified in
  * the $schema property. This is usefull if you doesn't have a clear idea of how
- * tthe document will look like
+ * the document will look like
  *
  * @package  Mongolid
  */
