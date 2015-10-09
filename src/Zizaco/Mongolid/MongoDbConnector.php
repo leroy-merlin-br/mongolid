78b979cf79efcdaa598a5e9f4bf4ad85806e02f2
Zizaco <zizaco@gmail.com>
2015-10-09 11:14:21 -0300

    Improved how MongoDbConnector handle connections

--- src/Zizaco/Mongolid/MongoDbConnector.php
+++ src/Zizaco/Mongolid/MongoDbConnector.php
@@ -3,38 +3,72 @@ namespace Zizaco\Mongolid;
 
 use MongoClient;
 use MongoDB;
+use MongoConnectionException;
 
 class MongoDbConnector
 {
     /**
-     * The connection that will be used.
+     * Stores the open connections with the connection string as the
+     * key.
      *
-     * @var MongoDB
+     * @var MongoClient[]
      */
-    public static $shared_connection;
+    public $connections = [];
 
     /**
-     * Returns the connection. If non existent then create it
+     * Default connection string that will be used if no connection
+     * string is specified
      *
-     * @var MongoDB
-     * @return MongoClient|MongoDB
+     * @var string
      */
-    public function getConnection($connectionString = '')
-    {
-        // If exists in $shared_connection, use it
-        if (MongoDbConnector::$shared_connection) {
-            $connection = MongoDbConnector::$shared_connection;
-        } else {
-            // Else, connect and place connection in $shared_connection
-            try {
-                $connection = new MongoClient($connectionString);
-            } catch (\MongoConnectionException $e) {
-                return trigger_error('Failed to connect with string: "' . $connectionString . '"');
-            }
+    public $defaultConnectionString;
 
-            MongoDbConnector::$shared_connection = $connection;
+    /**
+     * Returns the MongoClient object (the database connection itself). It will create one
+     * based in the given connection string if it doesn't exists.
+     *
+     * @var string $connectionString
+     *
+     * @return MongoClient
+     */
+    public function getConnection($connectionString = null)
+    {
+        $connectionString = $this->checkDefaultConnectionString($connectionString);
+
+        if (isset($this->connections[$connectionString]) && $this->connections[$connectionString]) {
+            return $this->connections[$connectionString];
         }
 
-        return $connection;
+        return $this->connections[$connectionString] = $this->newMongoClient($connectionString);
+    }
+
+    /**
+     * Figure out if the default connection string should be set or used.
+     * Returns a connection string
+     *
+     * @param  string $connectionString
+     *
+     * @return string  The connection string that should actually be used
+     */
+    protected function checkDefaultConnectionString($connectionString)
+    {
+        if (!$connectionString && !$this->defaultConnectionString) {
+            throw new MongoConnectionException("No connection string has been specified");
+        }
+
+        if (!$this->defaultConnectionString) {
+            $this->defaultConnectionString = $connectionString;
+        }
+
+        if (!$connectionString) {
+            return $this->defaultConnectionString;
+        }
+
+        return $connectionString;
+    }
+
+    protected function newMongoClient($connectionString)
+    {
+        return new MongoClient($connectionString);
     }
 }
