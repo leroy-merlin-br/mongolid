5555cdbbd3b339f38a88770458bd2bc1b2cd89f6
gmsantos <gabriel.ms1@hotmail.com>
2015-11-26 10:18:13 -0200

    Fix tests

--- tests/SequenceTest.php
+++ tests/SequenceTest.php
@@ -6,27 +6,40 @@ use Mockery as m;
 class SequenceTest extends PHPUnit_Framework_TestCase
 {
 
+    protected $sequence;
+
+    public function setUp()
+    {
+
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        m::close();
+    }
+
     public function testShouldReturnNextSequenceNumber()
     {
         // Set
         $connector = m::mock('Zizaco\Mongolid\MongoDbConnector');
-        $collection = m::mock('MongoCollection');
+        $mongoCollection = m::mock('MongoCollection');
+
+        $connector->shouldReceive('getConnection')
+            ->once()
+            ->andReturn($mongoCollection);
 
         $sequence = m::mock('Zizaco\Mongolid\Sequence' . '[collection]', array($connector, 'database'));
         $sequence->shouldAllowMockingProtectedMethods();
 
         $sequence->shouldReceive('collection')
             ->once()
-            ->andReturn($collection);
+            ->andReturn($mongoCollection);
 
-        $sequence->shouldAllowMockingProtectedMethods();
-
-        $sequenceName = 'orderId';
-
-        $collection->shouldReceive('findAndModify')
+        $mongoCollection->shouldReceive('findAndModify')
             ->once()
             ->with(
-                array('_id' => $sequenceName),
+                array('_id' => 'orderId'),
                 array('$inc' => array('seq' => 1)),
                 null,
                 array(
@@ -37,7 +50,25 @@ class SequenceTest extends PHPUnit_Framework_TestCase
             ->andReturn(array('seq' => 2));
 
         // Expected
-        $this->assertEquals(2, $sequence->getNextValue($sequenceName));
+        $this->assertEquals(2, $sequence->getNextValue('orderId'));
     }
 
+    public function testShouldReturnAMongoCollection()
+    {
+        $connector = m::mock('Zizaco\Mongolid\MongoDbConnector');
+        $connection = m::mock('ConnectionMock');
+        $database = m::mock('DatabaseMock');
+        $collection = m::mock('MongoCollection');
+
+        $connection->database = $database;
+        $database->mongolid_sequences = $collection;
+
+        $connector->shouldReceive('getConnection')
+            ->once()
+            ->andReturn($connection);
+
+        $sequence = m::mock('Zizaco\Mongolid\Sequence' . '[collection]', array($connector, 'database'));
+
+        $this->assertEquals($collection, $sequence->collection());
+    }
 }
