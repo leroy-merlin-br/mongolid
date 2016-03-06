25a42faf2ddedf81308be6ecdc1f722c15ab410c
Zizaco Zizuini <zizaco@gmail.com>
2016-03-06 11:03:40 -0300

    Added new Model\Attributes trait. This trait adds attribute getter, setters and fill following rules by the model properties

---
+++ src/Mongolid/Model/Attributes.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Mongolid\Model;
+
+/**
+ * This trait adds attribute getter, setters and also a usefull
+ * `fill` method that can be used with $fillable and $guarded
+ * properties to make sure that only the correct attributes
+ * will be set.
+ *
+ * It is supossed to be used in model classes in general
+ *
+ * @package  Mongolid
+ */
+trait Attributes
+{
+    /**
+     * The model's attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [];
+    /**
+     * The model attribute's original state.
+     *
+     * @var array
+     */
+    protected $original = [];
+    /**
+     * Once you put at least one string in this array, only
+     * the attributes especified here will be changed
+     * with the setAttributes method.
+     *
+     * @var array
+     */
+    public $fillable = [];
+    /**
+     * The attributes that aren't mass assignable. The oposite
+     * to the fillable array;
+     *
+     * @var array
+     */
+    public $guarded = [];
+
+    /**
+     * Get an attribute from the model.
+     *
+     * @param  string $key
+     *
+     * @return mixed
+     */
+    public function getAttribute($key)
+    {
+        $inAttributes = array_key_exists($key, $this->attributes);
+        if ($inAttributes) {
+            return $this->attributes[$key];
+        } elseif ($key == 'attributes') {
+            return $this->attributes;
+        } else {
+            return null;
+        }
+    }
+    /**
+     * Get all attributes from the model.
+     *
+     * @return mixed
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * Set the model attributes using an array
+     *
+     * @param  array $input
+     *
+     * @return void
+     */
+    public function fill($input)
+    {
+        foreach ($input as $key => $value) {
+            if ((empty($this->fillable) or in_array($key, $this->fillable)) && ! in_array($key, $this->guarded)) {
+                $this->setAttribute($key, $value);
+            }
+        }
+    }
+
+    /**
+     * Set a given attribute on the model.
+     *
+     * @param  string $key
+     */
+    public function cleanAttribute($key)
+    {
+        unset($this->attributes[$key]);
+    }
+
+    /**
+     * Set a given attribute on the model.
+     *
+     * @param  string $key
+     * @param  mixed  $value
+     *
+     * @return void
+     */
+    public function setAttribute($key, $value)
+    {
+        $this->attributes[$key] = $value;
+    }
+
+    /**
+     * Dynamically retrieve attributes on the model.
+     *
+     * @param  string $key
+     *
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this->getAttribute($key);
+    }
+    /**
+     * Dynamically set attributes on the model.
+     *
+     * @param  string $key
+     * @param  mixed  $value
+     *
+     * @return void
+     */
+    public function __set($key, $value)
+    {
+        // Set attribute
+        $this->setAttribute($key, $value);
+    }
+
+    /**
+     * Determine if an attribute exists on the model.
+     *
+     * @param  string $key
+     *
+     * @return bool
+     */
+    public function __isset($key)
+    {
+        return isset($this->attributes[$key]);
+    }
+    /**
+     * Unset an attribute on the model.
+     *
+     * @param  string $key
+     *
+     * @return void
+     */
+    public function __unset($key)
+    {
+        unset($this->attributes[$key]);
+    }
+}
---
+++ tests/Mongolid/Model/AttributesTest.php
@@ -0,0 +1,185 @@
+<?php
+namespace Mongolid\Model;
+
+use TestCase;
+use Mockery as m;
+use Mongolid\Container\Ioc;
+use stdClass;
+
+class AttributesTest extends TestCase
+{
+    public function tearDown()
+    {
+        parent::tearDown();
+        m::close();
+    }
+
+    public function testShouldHaveDynamicSetters()
+    {
+        // Arrange
+        $model = new _stubAttributes;
+        $childObj = new stdClass;
+
+        // Assert
+        $model->name = 'John';
+        $model->age = 25;
+        $model->child = $childObj;
+        $this->assertAttributeEquals(
+            [
+                'name' => 'John',
+                'age' => 25,
+                'child' => $childObj,
+            ],
+            'attributes',
+            $model
+        );
+    }
+
+    public function testShouldHaveDynamicGetters()
+    {
+        // Arrange
+        $model = new _stubAttributes;
+        $childObj = new stdClass;
+        $this->setProtected(
+            $model,
+            'attributes',
+            [
+                'name' => 'John',
+                'age' => 25,
+                'child' => $childObj,
+            ]
+        );
+
+        // Assert
+        $this->assertEquals('John', $model->name);
+        $this->assertEquals(25, $model->age);
+        $this->assertEquals($childObj, $model->child);
+        $this->assertEquals(null, $model->nonexistant);
+    }
+
+    public function testShouldCheckIfAttributeIsSet()
+    {
+        // Arrange
+        $model = new _stubAttributes;
+        $this->setProtected(
+            $model,
+            'attributes',
+            ['name' => 'John',]
+        );
+
+        // Assert
+        $this->assertTrue(isset($model->name));
+        $this->assertFalse(isset($model->nonexistant));
+    }
+
+    public function testShouldUnsetAttributes()
+    {
+        // Arrange
+        $model = new _stubAttributes;
+        $this->setProtected(
+            $model,
+            'attributes',
+            [
+                'name' => 'John',
+                'age' => 25,
+            ]
+        );
+
+        // Assert
+        unset($model->age);
+        $this->assertAttributeEquals(
+            [
+                'name' => 'John',
+            ],
+            'attributes',
+            $model
+        );
+    }
+
+    /**
+     * @dataProvider getFillableOptions
+     */
+    public function testShouldFillOnlyPermittedAttributes(
+        $fillable,
+        $guarded,
+        $input,
+        $expected
+    ) {
+        // Arrange
+        $model = new _stubAttributes;
+        $this->setProtected($model, 'fillable', $fillable);
+        $this->setProtected($model, 'guarded', $guarded);
+
+        // Assert
+        $model->fill($input);
+        $this->assertAttributeEquals($expected, 'attributes', $model);
+    }
+
+    public function getFillableOptions()
+    {
+        return [
+            // $fillable = []; $guarded = []
+            'all empty' => [
+                'fillable' => [],
+                'guarded' => [],
+                'input' => [
+                    'name' => 'John',
+                    'age' => 25,
+                    'sex' => 'male',
+                ],
+                'expected' => [
+                    'name' => 'John',
+                    'age' => 25,
+                    'sex' => 'male',
+                ],
+            ],
+
+            // $fillable = ['name']; $guarded = []
+            'with fillable' => [
+                'fillable' => ['name'],
+                'guarded' => [],
+                'input' => [
+                    'name' => 'John',
+                    'age' => 25,
+                    'sex' => 'male',
+                ],
+                'expected' => [
+                    'name' => 'John',
+                ],
+            ],
+
+            // $fillable = []; $guarded = []
+            'with guarded' => [
+                'fillable' => [],
+                'guarded' => ['sex'],
+                'input' => [
+                    'name' => 'John',
+                    'age' => 25,
+                    'sex' => 'male',
+                ],
+                'expected' => [
+                    'name' => 'John',
+                    'age' => 25,
+                ],
+            ],
+
+            // $fillable = ['name', 'sex']; $guarded = ['sex']
+            'with both' => [
+                'fillable' => ['name', 'sex'],
+                'guarded' => ['sex'],
+                'input' => [
+                    'name' => 'John',
+                    'age' => 25,
+                    'sex' => 'male',
+                ],
+                'expected' => [
+                    'name' => 'John',
+                ],
+            ],
+        ];
+    }
+}
+
+class _stubAttributes {
+    use Attributes;
+}
