7ab39a5a46884329cf0dc27f03a07c1a18e130c0
GuilhermeGuitte <guilherme.guitte@gmail.com>
2016-06-23 17:13:08 -0300

    Added $force parameter into fill() in order to be possible force fill variable if necessary

--- src/Mongolid/Model/Attributes.php
+++ src/Mongolid/Model/Attributes.php
@@ -81,10 +81,10 @@ trait Attributes
      *
      * @return void
      */
-    public function fill(array $input)
+    public function fill(array $input, bool $force = false)
     {
         foreach ($input as $key => $value) {
-            if ((empty($this->fillable) || in_array($key, $this->fillable)) && ! in_array($key, $this->guarded)) {
+            if ($force || (empty($this->fillable) || in_array($key, $this->fillable)) && ! in_array($key, $this->guarded)) {
                 $this->setAttribute($key, $value);
             }
         }
@@ -93,9 +93,8 @@ trait Attributes
     /**
      * Set a given attribute on the model.
      *
-     * @param  string $key Name of the attribute to be unset.
+     * @param string $key Name of the attribute to be unset.
      *
-     * @return void
      */
     public function cleanAttribute(string $key)
     {
