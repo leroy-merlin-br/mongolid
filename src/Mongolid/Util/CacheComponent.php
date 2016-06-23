39f73babb98c50b7e6785f501817c737d9354148
Zizaco Zizuini <zizaco@gmail.com>
2016-06-23 18:31:17 -0300

    Added CacheComponent in order to be able to cache queries and improve performance of document references.

---
+++ src/Mongolid/Util/CacheComponent.php
@@ -0,0 +1,84 @@
+<?php
+namespace Mongolid\Util;
+
+/**
+ * CacheComponent will cache values for later use based in "key, value"
+ * approach.
+ */
+class CacheComponent
+{
+    /**
+     * The array of stored values.
+     *
+     * @var array
+     */
+    protected $storage = [];
+
+    /**
+     * Time to live of each stored value.
+     *
+     * @var array
+     */
+    protected $ttl = [];
+
+    /**
+     * Retrieve an item from the cache by key.
+     *
+     * @param  string $key Cache key of the item to be retrieved.
+     * @return mixed
+     */
+    public function get(string $key)
+    {
+        if ($this->has($key)) {
+            return $this->storage[$key];
+        }
+    }
+
+    /**
+     * Store an item in the cache for a given number of minutes.
+     *
+     * @param string $key     Cache key of the item.
+     * @param mixed  $value   Value being stored in cache.
+     * @param float  $minutes Cache ttl.
+     *
+     * @return void
+     */
+    public function put(string $key, $value, float $minutes)
+    {
+        $this->storage[$key] = $value;
+        $this->ttl[$key] = $this->time() + 60 * $minutes;
+    }
+
+    /**
+     * Determine if an item exists in the cache. This method will also check
+     * if the ttl of the given cache key has been expired and will free the
+     * memory if so.
+     *
+     * @param  string $key Cache key of the item.
+     *
+     * @return bool Has cache key.
+     */
+    public function has(string $key): bool
+    {
+        if (array_key_exists($key, $this->ttl) &&
+            $this->time() - $this->ttl[$key] > 0
+        ) {
+            unset($this->ttl[$key]);
+            unset($this->storage[$key]);
+            return false;
+        }
+
+        return array_key_exists($key, $this->storage);
+    }
+
+    /**
+     * Return the current time in order to check ttl.
+     *
+     * @codeCoverageIgnore
+     * @return integer Return current Unix timestamp.
+     */
+    protected function time()
+    {
+        return time();
+    }
+}
---
+++ tests/Mongolid/Util/CacheComponentTest.php
@@ -0,0 +1,67 @@
+<?php
+namespace Mongolid\Util;
+
+use Mockery as m;
+use TestCase;
+
+class CacheComponentTest extends TestCase
+{
+    /**
+     * Current time that will be retrieved by CacheComponent::time()
+     * @var integer
+     */
+    public $time = 1466710000;
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        m::close();
+    }
+
+    public function testShouldPutAndRetrieve()
+    {
+        // Arrange
+        $cacheComponent = $this->getCacheComponent();
+
+        // Assertion
+        $cacheComponent->put('saveThe', 'bacon', 1); // 1 minute of ttl
+        $this->tick(30); // After 30 seconds
+        $this->assertTrue($cacheComponent->has('saveThe'));
+        $this->assertEquals('bacon', $cacheComponent->get('saveThe'));
+    }
+
+    public function testShouldExpireCachedValues()
+    {
+        // Arrange
+        $cacheComponent = $this->getCacheComponent();
+        $cacheComponent->put('saveThe', 'bacon', 1); // 1 minute of ttl
+
+        // Act
+        $this->tick(61); // After 61 seconds
+
+        // Assertion
+        $this->assertFalse($cacheComponent->has('saveThe'));
+        $this->assertNull($cacheComponent->get('saveThe'));
+    }
+
+    protected function getCacheComponent()
+    {
+        $test = $this;
+        $cacheComponent = m::mock(CacheComponent::class.'[time]');
+        $cacheComponent->shouldAllowMockingProtectedMethods();
+        $cacheComponent->shouldReceive('time')
+            ->andReturnUsing(function () use ($test) {
+                return $test->time;
+            });
+
+        return $cacheComponent;
+    }
+
+    /**
+     * Skips $seconds of time
+     */
+    protected function tick($seconds)
+    {
+        $this->time += $seconds;
+    }
+}
