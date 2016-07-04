e9384510d91ae5e3b986de56ace5332a6953dc8c
Zizaco Zizuini <zizaco@gmail.com>
2016-07-04 10:57:24 -0300

    Added new ObjectId utilities class.

---
+++ tests/Mongolid/Util/ObjectIdUtilsTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Mongolid\Util;
+
+use TestCase;
+use MongoDB\BSON\ObjectID;
+
+class ObjectIdUtilsTest extends TestCase
+{
+    /**
+     * @dataProvider objectIdStringScenarios
+     */
+    public function testShouldEvaluateIfStringIsAnObjectid($value, $expectation)
+    {
+        $this->assertEquals($expectation, ObjectIdUtils::isObjectId($value));
+    }
+
+    public function objectIdStringScenarios()
+    {
+        return [
+            // [Value, Expectation],
+            ['577a68c44d3cec1f6c7796a2', true],
+            ['577a68d24d3cec1f817796a5', true],
+            ['577a68d14d3cec1f6d7796a3', true],
+            ['507f1f77bcf86cd799439011', true],
+            ['507f191e810c19729de860ea', true],
+            [new ObjectID, true],
+            ['1', false],
+            ['507f191e810c197', false],
+            ['123456', false],
+            ['abcdefgh1234567890123456', false],
+            ['+07f191e810c19729de860ea', false],
+            [1234567, false],
+            [0.5, false],
+        ];
+    }
+}
