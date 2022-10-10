3c4b670d4a384e65e3ecef4fb93e0f81880460f7
Diego Felix <diegoflx.oliveira@gmail.com>
2022-10-10 12:28:20 -0300

    feat: create scenarios on references one relation

---
+++ tests/Unit/Model/HasLegacyRelationTraitTest.php
@@ -0,0 +1,97 @@
+<?php
+namespace Mongolid\Model;
+
+use Mockery as m;
+use MongoDB\BSON\ObjectId;
+use Mongolid\Cursor\EmbeddedCursor;
+use Mongolid\Query\Builder;
+use Mongolid\TestCase;
+use Mongolid\Tests\Stubs\EmbeddedUser;
+use Mongolid\Tests\Stubs\Price;
+use Mongolid\Tests\Stubs\Product;
+use Mongolid\Tests\Stubs\ReferencedUser;
+use Mongolid\Util\CacheComponent;
+use Mongolid\Util\CacheComponentInterface;
+
+final class HasLegacyRelationTraitTest extends TestCase
+{
+    /**
+     * @dataProvider referencesOneScenarios
+     */
+    public function testReferenceOneShouldNotHitCache($fieldValue, array $expectedQuery): void
+    {
+        // Set
+        $model = new Product();
+        $model->_id = $fieldValue;
+        $expected = new Price();
+        $priceModel = $this->instance(Price::class, m::mock(Price::class)->makePartial());
+        $cacheComponent = $this->instance(
+            CacheComponentInterface::class,
+            m::mock(CacheComponent::class)->makePartial()
+        );
+
+        // Expectations
+        $cacheComponent->expects()
+            ->get("prices:$fieldValue")
+            ->andReturnNull();
+
+        $priceModel->expects('first')
+            ->with($expectedQuery, [], true)
+            ->andReturn($expected);
+
+        // Actions
+        $result = $model->price();
+
+        // Assertions
+        $this->assertSame($expected, $result);
+    }
+
+    /**
+     * @dataProvider referencesOneScenarios
+     */
+    public function testReferenceOneShouldNotHitDatabase($fieldValue, array $expectedQuery): void
+    {
+        // Set
+        $model = new Product();
+        $model->_id = $fieldValue;
+        $expected = new Price();
+        $builder = $this->instance(Price::class, m::mock(Price::class)->makePartial());
+        $cacheComponent = $this->instance(
+            CacheComponentInterface::class,
+            m::mock(CacheComponent::class)->makePartial()
+        );
+
+        // Expectations
+        $cacheComponent->expects()
+            ->get("prices:$fieldValue")
+            ->andReturn($expected);
+
+        $builder->expects('first')
+            ->with($expectedQuery, [], true)
+            ->never();
+
+        // Actions
+        $result = $model->price();
+
+        // Assertions
+        $this->assertSame($expected, $result);
+    }
+
+    public function referencesOneScenarios(): array
+    {
+        return [
+            'referenced by string id' => [
+                'fieldValue' => 'abc123',
+                'expectedQuery' => ['_id' => 'abc123'],
+            ],
+            'referenced by objectId represented as string' => [
+                'fieldValue' => '577afb0b4d3cec136058fa82',
+                'expectedQuery' => ['_id' => new ObjectId('577afb0b4d3cec136058fa82')],
+            ],
+            'referenced by an objectId itself' => [
+                'fieldValue' => new ObjectId('577afb0b4d3cec136058fa82'),
+                'expectedQuery' => ['_id' => new ObjectId('577afb0b4d3cec136058fa82')],
+            ],
+        ];
+    }
+}
